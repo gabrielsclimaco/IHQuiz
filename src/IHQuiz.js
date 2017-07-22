1af28e74fb704fc62759e968d90090db59e64c11
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-22 18:11:55 -0300

    🎨 Structure component

--- src/IHQuiz.js
+++ src/IHQuiz.js
@@ -1,54 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
-  View
+  View,
+  Image,
 } from 'react-native';
+
+import Style from './style/IHQuizStyle'
 
 export default class IHQuiz extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={Style.container}>
+        <View style={Style.title}>
+          <Image
+            source={require('../static/assets/idea.png')}
+            style={Style.titleImage}
+          />
+          <Text style={Style.titleText}>IHQuiz</Text>
+        </View>
+        <View style={Style.buttonView}>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 AppRegistry.registerComponent('IHQuiz', () => IHQuiz);
-
