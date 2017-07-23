92b3ebbfb561fc3a85d56ba01bda9cdd265a9ac1
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-23 15:21:59 -0300

    💄 Add buttons

--- src/IHQuiz.js
+++ src/IHQuiz.js
@@ -4,11 +4,17 @@ import {
   Text,
   View,
   Image,
+  TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import Style from './style/IHQuizStyle'
 
 export default class IHQuiz extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <View style={Style.container}>
@@ -20,6 +26,26 @@ export default class IHQuiz extends Component {
           <Text style={Style.titleText}>IHQuiz</Text>
         </View>
         <View style={Style.buttonView}>
+          <TouchableOpacity
+            style={Style.button}
+            onPress={() => {Alert.alert('battle')}}
+          >
+            <Image
+              source={require('../static/assets/sword-battle.png')}
+              style={Style.buttonImage}
+            />
+            <Text style={Style.buttonTitle}>Batalhar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={Style.button}
+            onPress={() => {Alert.alert('train')}}
+          >
+            <Image
+              source={require('../static/assets/sword.png')}
+              style={Style.buttonImage}
+            />
+          <Text style={Style.buttonTitle} >Treinar</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
--- src/style/IHQuizStyle.js
+++ src/style/IHQuizStyle.js
@@ -18,8 +18,8 @@ const Style = StyleSheet.create({
     alignItems: 'center',
   },
   titleImage: {
-    width: responsiveWidth(15),
-    height: responsiveHeight(10),
+    width: responsiveWidth(24),
+    height: responsiveHeight(15),
     paddingLeft: 10,
   },
   titleText: {
@@ -29,7 +29,30 @@ const Style = StyleSheet.create({
   },
   buttonView: {
     flex: 6,
+    height: responsiveHeight(100),
+    width: responsiveWidth(100),
+    justifyContent: 'space-around',
+    alignItems: 'center',
   },
+  button: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: '#fafafa',
+    borderColor: '#384684',
+    borderWidth: 2,
+    padding: 20,
+    width: responsiveWidth(70),
+  },
+  buttonImage: {
+    height: responsiveHeight(9),
+    width: responsiveWidth(16),
+  },
+  buttonTitle: {
+    fontSize: responsiveFontSize(4),
+    fontWeight: '500',
+  }
 })
 
 export default Style
