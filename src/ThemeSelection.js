ffab19813f9ef2de44a1d3be21c4fdc8132086a4
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-25 01:36:45 -0300

    💄 Adds spining animation

--- src/ThemeSelection.js
+++ src/ThemeSelection.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
 import {
-  Text,
   View,
+  Animated,
+  Image,
+  Easing,
 } from 'react-native';
+
+import Style from './style/ThemeSelectionStyle'
 
 export default class ThemeSelection extends Component {
   static navigationOptions = {
     title: 'Seleção de tema',
   };
 
+  constructor(props) {
+    super(props);
+
+    this.spinValue = new Animated.Value(0);
+  }
+
+  componentDidMount () {
+    this.spin()
+  }
+  spin () {
+    this.spinValue.setValue(0)
+    Animated.timing(
+      this.spinValue,
+      {
+        toValue: 2,
+        duration: 4000,
+        easing: Easing.linear
+      }
+    ).start(() => this.spin())
+  }
+
   render() {
+    const spin = this.spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '720deg']
+    })
     return (
-      <View>
-        <Text>foi</Text>
+      <View style={Style.container}>
+        <Animated.Image
+          style={{
+            width: 227,
+            height: 200,
+            transform: [{rotate: spin}] }}
+            source={{uri: 'https://s3.amazonaws.com/media-p.slid.es/uploads/al'+
+              'exanderfarennikov/images/1198519/reactjs.png'}}
+        />
       </View>
     )
   }
---
+++ src/style/ThemeSelectionStyle.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import {
+  responsiveHeight,
+  responsiveWidth,
+  responsiveFontSize,
+} from 'react-native-responsive-dimensions';
+
+const Style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  }
+})
+
+export default Style
