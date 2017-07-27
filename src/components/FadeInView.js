93b3745c551a6f58a3540291caf4dcad422a15e6
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-27 16:27:21 -0300

    🎨 Add a section for components Add a fade in component

---
+++ src/components/FadeInView.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Animated, Text, View } from 'react-native';
+
+export default class FadeInView extends Component {
+  state = {
+    fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
+  }
+
+  componentDidMount() {
+    Animated.timing(                  // Animate over time
+      this.state.fadeAnim,            // The animated value to drive
+      {
+        toValue: 1,                   // Animate to opacity: 1 (opaque)
+        duration: this.props.duration,              // Make it take a while
+      }
+    ).start();                        // Starts the animation
+  }
+
+  render() {
+    let { fadeAnim } = this.state;
+
+    return (
+      <Animated.View                 // Special animatable View
+        style={{
+          ...this.props.style,
+          opacity: fadeAnim,         // Bind opacity to animated value
+        }}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
